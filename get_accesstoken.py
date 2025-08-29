b1af9949ed0870155552b25c77055ff2ab904a50
kutluege <69218899+kutluege@users.noreply.github.com>
2025-08-29 13:43:42 +0300

    Update get_accesstoken.py

--- get_accesstoken.py
+++ get_accesstoken.py
@@ -7,10 +7,8 @@ import os
 #redirect_uri = os.getenv("REDIRECT_URI")  # Replace with your Redirect URI in .env
 
 client_id = "f29ee281b7664d65ad23ecc58965e61d"
-client_secret ="55f57bd1a7c14c3ba3e05657f9f19294"
 redirect_uri = "https://llmspotify.streamlit.app/"
 
-code = "AQBbyZxUMmkSg9qS72h-ezaqT6r8-M1xTEJkigIR7WmArKuV8By2AIcC_UjxtCp8ou83r93SRtyFH2yBD2p-bSPSAkhVICwRd4fdIZfE30gfrKqnp1Ko82VPUB1NXXENn_U7j2v7XvXT87cRbNflWgWMe4q8wGIfeTeF1oFd_WQOy61iKT0SMqKJfTwv-vrelXGBQEMFs1PpM-B7"
 
 def get_access_token(code):
     url = "https://accounts.spotify.com/api/token"
